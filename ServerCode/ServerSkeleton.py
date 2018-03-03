eca78080bf2c351115c3d4d698e210d35b54a207
unknown <fmurray1@mix.wvu.edu>
2018-03-02 18:20:25 -0600

    got the graphing working and relative distances

--- ServerCode/ServerSkeleton.py
+++ ServerCode/ServerSkeleton.py
@@ -1,25 +1,30 @@
 import socket
 import sys
 from enum import Enum
+import matplotlib.pyplot as plt
+import networkx as nx
+from sets import Set
 
 class BeaconData(Enum):
-	TYPE = 0
-	BSSID = 1
-	MACADDRESS = 2
-	CHANNEL = 3
-	RSSI = 4
-	
+    TYPE = 0
+    BSSID = 1
+    MACADDRESS = 2
+    CHANNEL = 3
+    RSSI = 4
+    
 class DeviceData(Enum):
-	TYPE = 0
-	MACADDRESS = 1
-	BSSID = 2
-	DESTMAC = 3
-	CHANNEL = 4
-	RSSI = 5
+    TYPE = 0
+    MACADDRESS = 1
+    BSSID = 2
+    DESTMAC = 3
+    CHANNEL = 4
+    RSSI = 5
 
-	
+G = nx.Graph()
+WiFlyNodes = Set()
 currentFly = 'C0FFEEBEA575'
-
+deviceDict = {}
+beaconDict = {}
 #This function will read the port off of the command line
 def readArgs():
 
@@ -47,57 +52,69 @@ def servListen(cmds):
             if len(new_data) == 0:
                 break
         try:
-            print(data.decode('utf-8'))
-        except:
-            print('ERROR: Data received was not UTF-8!\n')
+            print("got data")
+            populateGraph(data.decode('utf-8'))
+        except Exception as e:
+            print(e)
         con.close()
 
-		
-def buildGraph(deviceDict, beaconDict, fliesDict):
-	try:
-		import matplotlib.pyplot as plt
-		import networkx as nx
-	except:
-		raise
-	
-	G=nx.Graph()
-	
-	for device, strength in deviceDict.iteritems():
-		G.add_edge(currentFly, device, weight=strength)
-	
-	for beacon, strength in beaconDict.iteritems():
-		G.add_edge(currentFly, beacon, weight=strength)
+        
+def buildGraph(deviceDict, beaconDict, currentFly):
+    
+    print('buildGraph')
+    
+    WiFlyNodes.update(currentFly)
+    for device, strength in deviceDict.iteritems():
+        G.add_edge(currentFly, device, weight=strength)
+    
+    for beacon, strength in beaconDict.iteritems():
+        G.add_edge(currentFly, beacon, weight=strength)
+        
+    print(G.edges())
 
-	pos=nx.spring_layout(G)
-	
-	nx.draw_networkx_nodes(G,pos,node_size=700)
-	
-	nx.draw_networkx_edges(G,pos, width=6)
-	
-	nx.draw_networkx_labels(G,pos,font_size=20,font_family='sans-serif')
-	
-	plt.axis('off')
-	
-	plt.show()
-					
-					
-	
-	
-def populatGraph(str):
-	ls = str.split('\n')
-	for s in ls:
-		s = s.split(':')
-		if "device" in s[DeviceData.TYPE]:
-			deviceDict[s[DeviceData.MACADDRESS]] = s[DeviceData.RSSI]
-		elif "beacon" in s:
-			beaconDict[s[BeaconData.MACADDRESS]] = s[BeaconData.RSSI]
-	buildGraph(deviceDict, beaconDict)
+    pos=nx.spring_layout(G)
+    
+    nx.draw_networkx_nodes(G,pos,node_size=25, node_color="g")
+    nx.draw_networkx_nodes(G,pos,node_list=WiFlyNodes, node_color="b", node_size=150)
+   
+    nx.draw_networkx_edges(G,pos, width=.5)
+    
+    nx.draw_networkx_labels(G,pos,font_size=6,font_family='sans-serif')
+    
+    plt.axis('off')
+    plt.draw()
+    plt.pause(0.05)
+                   
+def populateGraph(str):
+    print('populateGraph')
+    ls = str.split('\n')
+    currentFly = ls[0]
+    currentFly = currentFly.replace(':','').lower()
+    print('currentFly: {}'.format(currentFly))
+    for s in ls[1:]:
+        s = s.strip().split(':')
+        print(s)
+        if len(s) >= 4:
+            if "DEVICE" in s[DeviceData.TYPE]:
+                deviceDict[s[DeviceData.MACADDRESS]] = s[DeviceData.RSSI]
+            elif "BEACON" in s:
+                beaconDict[s[BeaconData.MACADDRESS]] = s[BeaconData.RSSI]
+    print(deviceDict)
+    print(beaconDict)
+    buildGraph(deviceDict, beaconDict, currentFly)
 
 
 if __name__ == '__main__':
+    """
+    test_dict = {'a':-70, 'b':-40}
+    test_beacon = {}
+    buildGraph(test_dict, test_beacon)
+    """
     cmds = readArgs()
     if cmds['port'] == 0:
         print("ERROR: No port was passed!\n")
     else:
         print("DEBUG: Listening on port: "+str(cmds['port']))
+        plt.ion()
+        plt.show()
         servListen(cmds)
